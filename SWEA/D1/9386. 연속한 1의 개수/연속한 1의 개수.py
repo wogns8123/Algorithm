7878f47e16a54ad5c5b56cba844cfb8411c1631b
wogns8123 <109336593+wogns8123@users.noreply.github.com>
2022-08-12 16:56:56 +0900

    [D1] Title: 연속한 1의 개수, Time: 165 ms, Memory: 60,100 KB -BaekjoonHub

---
+++ SWEA/D1/9386. 연속한 1의 개수/연속한 1의 개수.py
@@ -0,0 +1,16 @@
+
+tc = int(input())
+
+for test in range(tc):
+    num = int(input())
+    arr = list(input())
+    count = 0
+    count_list = []
+    for i in range(num):
+        if arr[i] == '1':
+            count+=1
+        else:
+            count = 0
+        count_list.append(count)
+
+    print(f'#{test+1} {max(count_list)}')
