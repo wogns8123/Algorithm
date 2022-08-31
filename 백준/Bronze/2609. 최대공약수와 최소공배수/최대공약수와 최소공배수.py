68ca5819211acfa4afa334bae85338404c28ff72
wogns8123 <109336593+wogns8123@users.noreply.github.com>
2022-08-31 22:41:28 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,6 +1,9 @@
-import math
-
 a, b = map(int, input().split())
+max_ = []
 
-print(math.gcd(a, b))
-print(math.lcm(a, b))
+for i in range(1, min(a, b) + 1):
+    if a % i == 0 and b % i == 0:
+        max_.append(i)
+
+print(max(max_))
+print(max(max_) * (a // max(max_)) * (b // max(max_)))
