44a5a98097b8f6baa5ce1a893f8bee8b396573ce
wogns8123 <wogns8123@naver.com>
2023-11-23 17:02:38 +0900

    [level unrated] Title: 배열 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.py
@@ -0,0 +1,13 @@
+def solution(arr1, arr2):
+    if len(arr1) == len(arr2):
+        if sum(arr1) > sum(arr2):
+            answer = 1
+        elif sum(arr1) < sum(arr2):
+            answer = -1
+        else:
+            answer = 0
+    elif len(arr1) > len(arr2):
+        answer = 1
+    else:
+        answer = -1
+    return answer
