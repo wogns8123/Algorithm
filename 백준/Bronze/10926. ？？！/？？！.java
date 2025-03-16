57af1f6557ae18195abae3538998f5dbb37c08db
wogns8123 <wogns8123@naver.com>
2025-03-16 18:38:15 +0900

    [Bronze V] Title: ??!, Time: 176 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        System.out.println(name + "??!");
+    }
+}
