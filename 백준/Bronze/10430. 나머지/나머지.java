029c78ecb755235bb18ce4c9c55f7119bae9d14e
wogns8123 <wogns8123@naver.com>
2025-03-16 18:49:31 +0900

    [Bronze V] Title: 나머지, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        String[] parts = input.split(" ");
+        int A = Integer.parseInt(parts[0]);
+        int B = Integer.parseInt(parts[1]);
+        int C = Integer.parseInt(parts[2]);
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+    }
+}
