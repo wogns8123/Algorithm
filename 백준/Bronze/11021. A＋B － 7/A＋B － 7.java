b859ca1d7f165ee7305b3f890f5c6e462df05b59
wogns8123 <wogns8123@naver.com>
2025-03-23 03:42:01 +0900

    [Bronze V] Title: A+B - 7, Time: 208 ms, Memory: 18624 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int testCase = scanner.nextInt();
+        for (int i = 0; i< testCase; i ++){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println("Case #"+(i+1)+": "+(a+b));
+        }
+    }
+}
