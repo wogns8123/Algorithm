3c91c96ec1fd9151811794e1f543c3c24783c7b3
wogns8123 <wogns8123@naver.com>
2025-03-23 18:45:41 +0900

    [Bronze IV] Title: 숫자의 합, Time: 172 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -8,13 +8,10 @@ public class Main {
         scan.nextLine();
         String word = scan.nextLine();
         int total = 0;
-        
-        char[] charArr = word.toCharArray();
-        
+
         for (int i = 0; i < word.length(); i++){
-//            int number = word.charAt(i);
-//            total += number;
-            total += Character.getNumericValue(charArr[i]);
+            int number = Character.getNumericValue(word.charAt(i));
+            total += number;
         }
         System.out.println(total);
     }
