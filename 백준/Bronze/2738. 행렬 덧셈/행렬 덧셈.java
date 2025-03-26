e9badeccfeb70817812386a754f182548206d0d0
wogns8123 <wogns8123@naver.com>
2025-03-26 22:59:22 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 416 ms, Memory: 22096 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,34 +1,25 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int m = scan.nextInt();
+        scan.nextLine();
         int[][] arr1 = new int[n][m];
         int[][] arr2 = new int[n][m];
 
         for(int i = 0; i< n; i++){
-            for(int j = 0; j< m; j++){
-                arr1[i][j] = scan.nextInt();
-            }
+            arr1[i] = Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
         for(int i = 0; i< n; i++){
-            for(int j = 0; j< m; j++){
-                arr2[i][j] = scan.nextInt();
-            }
+            arr2[i] = Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
-        int[][] result = new int[n][m];
-        for(int i = 0; i< n; i++){
-            for(int j = 0; j< m; j++){
-                result[i][j] = arr1[i][j] + arr2[i][j];
-            }
-        }
-        for(int i = 0; i < n ; i++){
-//            System.out.println(Arrays.toString(result[i]));
-            for(int j = 0; j<m; j++){
-                System.out.print(result[i][j] + " ");
+        for(int i = 0; i<n;i++){
+            for(int j =0 ; j< m; j++){
+                System.out.print(arr1[i][j] + arr2[i][j] + " ");
             }
             System.out.println();
         }
