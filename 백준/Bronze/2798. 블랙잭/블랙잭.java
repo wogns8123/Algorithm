17d4d546555868ed4adc4320aab5e2d8265bedad
wogns8123 <wogns8123@naver.com>
2025-03-31 20:17:15 +0900

    [Bronze II] Title: 블랙잭, Time: 112 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,26 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
-
-import static java.lang.Math.max;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-        int goal = scan.nextInt();
-        scan.nextLine();
-        int[] arr = Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int maxgoal = 0;
-        for(int i = 0 ; i<number-2;i++){
-            for(int j =i+1 ; j<number-1;j++){
-                for(int k = j+1; k<number;k++){
-                    int sum = arr[i] + arr[j] + arr[k];
-                    if (sum <= goal){
-                        maxgoal = max(maxgoal, sum);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int number = Integer.parseInt(st.nextToken());
+        int maxNumber = Integer.parseInt(st.nextToken());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int total = 0;
+        for(int i = 0; i< arr.length-2; i++){
+            for(int j = i+1; j<arr.length-1;j++){
+                for(int k = j+1; k<arr.length;k++){
+                    if (total < arr[i]+arr[j]+arr[k] && arr[i]+arr[j]+arr[k] <=maxNumber){
+                        total = arr[i]+arr[j]+arr[k];
                     }
                 }
             }
         }
-        System.out.println(maxgoal);
+        System.out.println(total);
+
     }
 }
