08a40d23c38db367836a2bf3e31399bd8e56c58b
wogns8123 <wogns8123@naver.com>
2025-04-05 03:22:16 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 168 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num =scan.nextInt();
+        long res = 1;
+        for(int i = num ; i>0;i--){
+            res *= i;
+        }
+        System.out.println(res);
+    }
+}
