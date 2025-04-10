f45b774ee2372dadd4012e8cb6bac9f464d2a1d3
wogns8123 <wogns8123@naver.com>
2025-04-10 13:55:06 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -3,27 +3,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
         int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] dp = new int[num];
-        Arrays.fill(dp, 1);
 
-        for(int i = 1; i<num; i++){
-            for(int j = 0; j<i; j++){
-                if(arr[j] < arr[i]){
-                    dp[i] = Math.max(dp[i], dp[j]+1);
+        ArrayList<Integer> dp = new ArrayList<>();
+        for(int i = 0; i< num; i++){
+            int now = arr[i];
+            if(dp.isEmpty() || dp.get(dp.size()-1) < now){
+                dp.add(now);
+            } else{
+                int left = 0;
+                int right = dp.size()-1;
+
+                while(left<right){
+                    int mid = (left + right) / 2;
+                    if(dp.get(mid) < now){
+                        left = mid+1;
+                    } else{
+                        right = mid;
+                    }
                 }
+                dp.set(left,now);
             }
         }
-        int max = Integer.MIN_VALUE;
-        for(int k : dp){
-            max = max<k ? k:max;
-        }
-        System.out.println(max);
+        System.out.println(dp.size());
     }
 }
