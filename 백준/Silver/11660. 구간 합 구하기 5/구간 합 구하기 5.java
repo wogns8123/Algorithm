d43b94680cfe7c718f90accfe9b4c3b0e43cc42d
wogns8123 <wogns8123@naver.com>
2025-04-12 22:47:12 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 736 ms, Memory: 121484 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,36 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N][N];
-        int[][] res = new int[N+1][N+1];
-        for(int i = 1; i <= N ; i++){
-            StringTokenizer arrNum = new StringTokenizer(br.readLine());
-            for(int j = 1; j<=N; j++){
-                res[i][j] = res[i][j-1] + Integer.parseInt(arrNum.nextToken());
-            }
-        }
         StringBuilder sb = new StringBuilder();
-        for(int i = 1; i<=M; i++){
-            int sum = 0;
-            StringTokenizer st2 = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st2.nextToken());
-            int y1 = Integer.parseInt(st2.nextToken());
-            int x2 = Integer.parseInt(st2.nextToken());
-            int y2 = Integer.parseInt(st2.nextToken());
-            for(int j = x1; j<=x2; j++){
-                sum = sum + res[j][y2] -res[j][y1-1];
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n + 1][n + 1];  // 원본 배열
+        int[][] dp = new int[n + 1][n + 1];   // 누적합 배열
+
+        // 원본 배열 입력
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
-            sb.append(sum+"\n");
         }
-        System.out.println(sb.toString());
+
+        // 2차원 누적합 배열 계산
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+                        - dp[i - 1][j - 1] + arr[i][j];
+            }
+        }
+        
+        // 쿼리 처리
+        for (int q = 0; q < m; q++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int sum = dp[x2][y2]
+                    - dp[x1 - 1][y2]
+                    - dp[x2][y1 - 1]
+                    + dp[x1 - 1][y1 - 1];
+
+            sb.append(sum).append('\n');
+        }
+
+        System.out.print(sb);
     }
 }
