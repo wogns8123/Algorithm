0455d34b49edd9d3bf03ab188e913aa9d47fd91c
wogns8123 <wogns8123@naver.com>
2025-04-14 08:49:35 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 716 ms, Memory: 109744 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -2,29 +2,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] word = br.readLine().split("");
+
+        String S = br.readLine();
+        int len = S.length();
+        int[][] prefix = new int[26][len];
+
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < 26; j++) {
+                if (i == 0) prefix[j][i] = 0;
+                else prefix[j][i] = prefix[j][i - 1];
+            }
+            prefix[S.charAt(i) - 'a'][i]++;
+        }
+
+
 
         int q = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i<q;i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String find = st.nextToken();
-
+            String[] input = br.readLine().split(" ");
+            char c = input[0].charAt(0);
+            int l = Integer.parseInt(input[1]);
+            int r = Integer.parseInt(input[2]);
             int count = 0;
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            for(int j = start ; j<=end; j++){
-                if (word[j].equals(find)){
-                    count ++;
-                }
-            }
 
-            System.out.println(count);
+            if ( l == 0){
+                count = prefix[c-'a'][r];
+            }
+            else{
+                count = prefix[c-'a'][r] - prefix[c-'a'][l-1];
+            }
+            sb.append(count).append("\n");
         }
+        System.out.println(sb.toString());
+
 
     }
 }
