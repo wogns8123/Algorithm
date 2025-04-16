29715312b79a51c26478b2c461901ba1b45d5a2d
wogns8123 <wogns8123@naver.com>
2025-04-16 15:58:05 +0900

    [Bronze IV] Title: 세수정렬, Time: 136 ms, Memory: 16640 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[1] + " " + arr[2]);
+    }
+}
