b3979b0da607d17d70d04a184c1e77c7ede40def
wogns8123 <wogns8123@naver.com>
2025-08-18 23:24:13 +0900

    [Bronze II] Title: 거리의 합, Time: 512 ms, Memory: 30456 KB -BaekjoonHub

---
+++ 백준/Bronze/2399. 거리의 합/거리의 합.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int[] x = new int[N];
+		long allDistance = 0;	// 모든 쌍의 거리를 더한 값
+		
+		for(int i=0; i<N; i++) 
+			x[i] = scan.nextInt();
+		
+		for(int i=0; i<x.length; i++) 
+			for(int j=0; j<x.length; j++) 
+				allDistance += Math.abs(x[i]-x[j]);
+		
+		System.out.println(allDistance);
+		scan.close();
+	}
+
+}
